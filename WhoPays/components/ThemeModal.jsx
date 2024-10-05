1fd4eb8f763ceb4d20f7f21d554502dc862c94f3
smootny <blazejewski@post.pl>
2024-10-05 20:38:21 +0200

    adjust images for themes

--- WhoPays/components/ThemeModal.jsx
+++ WhoPays/components/ThemeModal.jsx
@@ -3,14 +3,10 @@ import { Modal, View, Text, StyleSheet } from 'react-native';
 import { ThemedButton } from "react-native-really-awesome-button";
 import Animated, { useSharedValue, useAnimatedStyle, withTiming, BounceIn } from 'react-native-reanimated';
 import ThemeTile from './ThemeTile';
+import { themes } from '../constants/themes';
 
 const ThemeModal = ({ visible, onClose, onSelectTheme }) => {
-    const themes = [
-        { label: 'Cloud Theme', value: 'cloud', image: require('../assets/themes/cloud_theme/sky.png') },
-        { label: 'Table Theme', value: 'table', image: require('../assets/themes/table_theme/table.png') },
-        { label: 'Mine Theme', value: 'mine', image: require('../assets/themes/mine_theme/mine.png') },
-        { label: 'Diamond Theme', value: 'diamond', image: require('../assets/themes/diamond_theme/jewelry.png') },
-    ];
+
 
     const backgroundOpacity = useSharedValue(0);
 
